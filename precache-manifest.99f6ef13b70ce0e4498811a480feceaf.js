f3b024fc6c7b87c585ee9c6c917001e2efd27157
jef1771 <james.ferris1996@gmail.com>
2020-11-22 21:04:52 -0500

    pushin

---
+++ precache-manifest.99f6ef13b70ce0e4498811a480feceaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e76df35694d767cc28aea36f71ee710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533ba7d5601a5eeab121",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad9076c6f6b854c4adf6",
+    "url": "/static/js/2.bedba037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bedba037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533ba7d5601a5eeab121",
+    "url": "/static/js/main.f9b1eb3d.chunk.js"
+  },
+  {
+    "revision": "0085e366dcc9bce7a17d",
+    "url": "/static/js/runtime-main.9e467b42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
